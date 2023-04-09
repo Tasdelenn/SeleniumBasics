239e0c7083e2e29fbc40137ba2ef58add6fe21ce
HaQaN <hakann.tasdelenn@gmail.com>
2023-04-09 18:16:11 +0300

    web drivers yolu düzeltildi.

--- src/Batch_03/Gun04/_01_OpeningWebsite.java
+++ src/Batch_03/Gun04/_01_OpeningWebsite.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class _01_OpeningWebsite {
     public static void main(String[] args) throws InterruptedException {
 
-        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.techno.study/");
 
