a3eed8f94556393b4e8f4c2b82aacdfd7fb7a67d
HaQaN <hakann.tasdelenn@gmail.com>
2023-04-09 21:11:26 +0300

    Day04 > No such element / Unable to locate element

---
+++ src/Batch_05/Gun04/_03_FindingByIdNotFoundException.java
@@ -0,0 +1,27 @@
+package Batch_05.Gun04;
+
+
+import Batch_05.Utlity.MyFunc;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class _03_FindingByIdNotFoundException {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://form.jotform.com/221934510376353"); // sayfaya gittim
+
+        try {
+            WebElement labelFirstName = driver.findElement(By.id("hataliLocator"));
+            System.out.println("labelFirstName.getText() = " + labelFirstName.getText());
+        } catch (Exception ex) {
+            System.out.println("WebElement bulunamadı " + ex.getMessage());
+        }
+
+        MyFunc.Bekle(3);
+        driver.quit();
+
+    }
+}
